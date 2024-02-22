c75fa0be21fd8dca34d76434392f587953b69920
RadiuxSource <157123399+RadiuxSource@users.noreply.github.com>
2024-02-22 12:43:51 +0530

    Update start.py

--- RadiuxMusic/plugins/bot/start.py
+++ RadiuxMusic/plugins/bot/start.py
@@ -24,7 +24,7 @@ from config import BANNED_USERS
 from strings import get_string
 
 
-@app.on_message(filters.command(["start"]) & filters.private & ~BANNED_USERS)
+@app.on_message(filters.command(["mstart"]) & filters.private & ~BANNED_USERS)
 @LanguageStart
 async def start_pm(client, message: Message, _):
     await add_served_user(message.from_user.id)
@@ -96,7 +96,7 @@ async def start_pm(client, message: Message, _):
             )
 
 
-@app.on_message(filters.command(["start"]) & filters.group & ~BANNED_USERS)
+@app.on_message(filters.command(["mstart"]) & filters.group & ~BANNED_USERS)
 @LanguageStart
 async def start_gp(client, message: Message, _):
     out = start_panel(_)
